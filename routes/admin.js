e0c3a09cd9b3945a0df4b6e8a77df1a02d765522
Psp32 <prempatro317@gmail.com>
2025-12-25 04:06:56 +0530

    added auth endpoints for user and admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,46 @@
 const {Router} = require('express')
 const adminRouter = Router()
+const {adminModel} = require('../db')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 // This route will cater - '/api/v1/admin/signup'
-adminRouter.post('/signup',(req,res)=>{
+adminRouter.post('/signup',async (req,res)=>{
+    const {email,password,firstName,lastName} = req.body
+
+    // TODO: add zod, hash password, put try catch
+    await adminModel.create({
+        email,
+        password,
+        firstName,
+        lastName
+    })
+
     res.json({
-        msg: "signup endpoint"
+        msg: "signup done"
     })
 })
 
 // This route will cater - '/api/v1/admin/signin'
-adminRouter.post('/signin',(req,res)=>{
+adminRouter.post('/signin',async (req,res)=>{
+    const {email,password} = req.body
 
+    const admin = await adminModel.findOne({
+        email,
+        password // after hashing, check pw check first 
+    })
+
+    console.log(admin)
+    if(admin){
+        const token = jwt.sign({id:admin._id},process.env.JWT_SECRET_ADMIN)
+        res.json({
+            token: token
+        })
+    }else{
+        res.status(403).json({
+            msg: "User doesn't exists"
+        })
+    }
 })
 
 // Has to put a middleware (adminAuth) here to check if this is an admin or not, for all the requests after this line.
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,48 @@
 const express = require('express')
 const userRouter = express.Router() // Router instance created
+const {userModel} = require('../db')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+userRouter.use(express.json())
 
 // This route will cater - '/api/v1/user/signup'
-userRouter.post('/signup',(req,res)=>{
+userRouter.post('/signup',async (req,res)=>{
+    const {email,password,firstName,lastName} = req.body
+
+    // TODO: add zod, hash password, put try catch
+    const user = await userModel.create({
+        email,
+        password,
+        firstName,
+        lastName
+    })
+
     res.json({
-        msg: "signup endpoint"
+        msg: "signup done"
     })
 })
 
 // This route will cater - '/api/v1/user/signin'
-userRouter.post('/signin',(req,res)=>{
+userRouter.post('/signin',async (req,res)=>{
+    const {email,password} = req.body
 
+    const user = await userModel.findOne({
+        email,
+        password // after hashing, check pw check first 
+    })
+
+    console.log(user)
+    if(user){
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET_USER)
+        res.json({
+            token: token
+        })
+    }else{
+        res.status(403).json({
+            msg: "User doesn't exists"
+        })
+    }
 })
 
 // This route will cater - '/api/v1/user/purchase'
